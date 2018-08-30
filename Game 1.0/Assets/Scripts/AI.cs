afd61ddce045e5ba8a3c4c2f45372ea747a5eaa1
Gabriel Leonardsson <gabriel.gonzalezleonardsson@elev.ga.lbs.se>
2018-08-30 11:03:32 +0200

    Fixade en AI (lil boii)
    Lil boii rör sig fram och tillbaka när an träffar väggar

---
+++ Game 1.0/Assets/Scripts/AI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    private Vector3 MovingDirection = Vector3.left;
+    public bool moveRight = false;
+
+    void FixedUpdate()
+    {
+        Movement();
+
+        if (MovingDirection == Vector3.left)
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
+        else if (MovingDirection == Vector3.right)
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+    }
+
+    void Movement()
+    {
+        if (moveRight == false)
+            MovingDirection = Vector3.left;
+        else if (moveRight == true)
+            MovingDirection = Vector3.right;
+
+        this.transform.Translate(MovingDirection * Time.smoothDeltaTime);
+    }
+}
