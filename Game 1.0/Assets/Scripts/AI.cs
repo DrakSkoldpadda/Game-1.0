d2df6b62c9bee5ca41d288262a398ee0fc0ec9eb
Gabriel Leonardsson <gabriel.gonzalezleonardsson@elev.ga.lbs.se>
2018-08-30 11:21:43 +0200

    Håller på med AI HP

--- Game 1.0/Assets/Scripts/AI.cs
+++ Game 1.0/Assets/Scripts/AI.cs
@@ -4,27 +4,50 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    private Vector3 MovingDirection = Vector3.left;
+    //AI name = Lil boii
+
+    [Header("MovingAtributes (DON'T TOUCH!)")]
     public bool moveRight = false;
+    private Vector3 MovingDirection = Vector3.left;
+    private float moveSpeed = 10f;
+
+    [Header("HealthAtributes (DON'T TOUCH!)")]
+    public int AIHP = 5;
 
     void FixedUpdate()
     {
         Movement();
 
-        if (MovingDirection == Vector3.left)
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        Health();
+    }
 
-        else if (MovingDirection == Vector3.right)
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+    void Health()
+    {
+
+        if (AIHP <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
     }
 
     void Movement()
     {
+        //Åt vilket håll Lil boii ska röra sig åt
         if (moveRight == false)
             MovingDirection = Vector3.left;
         else if (moveRight == true)
             MovingDirection = Vector3.right;
 
-        this.transform.Translate(MovingDirection * Time.smoothDeltaTime);
+        //Bestämmer åt vilket håll Lil Boii ska kolla åt, checkas med åt vilket håll Lil boii går åt
+        if (MovingDirection == Vector3.left)
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        else if (MovingDirection == Vector3.right)
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        //Hur snabb Lil boii är
+        this.transform.Translate(MovingDirection * moveSpeed * Time.smoothDeltaTime);
     }
 }
