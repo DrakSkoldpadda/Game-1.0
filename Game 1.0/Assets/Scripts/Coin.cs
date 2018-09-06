9b169b7ccea357038bb57eb3f6be06f37ad1808f
Viktor Åkerberg <Viktor.akerberg@elev.ga.lbs.se>
2018-09-06 10:43:15 +0200

    coin script
    klar

--- Game 1.0/Assets/Scripts/Coin.cs
+++ Game 1.0/Assets/Scripts/Coin.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     private int worth = 5;
+    private GameObject gameController;
+
+    private void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController");
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        gameController.GetComponent<GameController>().coins += worth;
         GameObject.Destroy(gameObject);
     }
 }
