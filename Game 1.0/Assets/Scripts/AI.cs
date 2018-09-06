803d4956825cbb6978b3cd7b21b06907d38e9754
Gabriel Leonardsson <gabriel.gonzalezleonardsson@elev.ga.lbs.se>
2018-09-06 09:12:25 +0200

    La till projektet igen.
    Detta projekt har mina changes i sig.

---
+++ Game 1.0/Assets/Scripts/AI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    //AI name = Lil boii
+
+    [Header("MovingAtributes (DON'T TOUCH!)")]
+    public bool moveRight = false;
+    private Vector3 MovingDirection = Vector3.left;
+    private float moveSpeed = 1.5f;
+
+    [Header("HealthAtributes (DON'T TOUCH!)")]
+    public int AIHP = 3;
+    private SpriteRenderer spriteRend;
+    private Color orange = new Color(255F, 100F, 10F);
+
+    //[Header("HealthAtributes (DON'T TOUCH!)")]
+    //public int AIHP = 5;
+
+    void FixedUpdate()
+    {
+        Movement();
+
+        //Health();
+    }
+
+    //void Health()
+    //{
+    //    if (AIHP == 2)
+    //    {
+    //        spriteRend.color = orange;
+    //    }
+    //    else if (AIHP == 1)
+    //    {
+    //        spriteRend.color = Color.red;
+    //    }
+    //    if (AIHP <= 0)
+    //        Die();
+    //}
+
+    //void Die()
+    //{
+    //    Destroy(gameObject);
+    //}
+
+    void Movement()
+    {
+        //Åt vilket håll Lil boii ska röra sig åt
+        if (moveRight == false)
+            MovingDirection = Vector3.left;
+        else if (moveRight == true)
+            MovingDirection = Vector3.right;
+
+        //Bestämmer åt vilket håll Lil Boii ska kolla åt, checkas med åt vilket håll Lil boii går åt
+        if (MovingDirection == Vector3.left)
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        else if (MovingDirection == Vector3.right)
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+
+        //Hur snabb Lil boii är
+        this.transform.Translate(MovingDirection * moveSpeed * Time.smoothDeltaTime);
+    }
+}
---
+++ Game 1.0/Assets/Scripts/AICollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICollider : MonoBehaviour
+{
+    public AI AIVariables;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            if (AIVariables.moveRight == false)
+                AIVariables.moveRight = true;
+            else
+                AIVariables.moveRight = false;
+        }
+
+        //if (collision.gameObject.CompareTag("Projectile"))
+        //{
+        //    AIVariables.AIHP -=
+        //}
+    }
+}
