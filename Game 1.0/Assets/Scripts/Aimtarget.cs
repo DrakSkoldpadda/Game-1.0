3a1800d738164e46aa7b7eff88260418562fb483
DotBlader <fredrik.ehrs@elev.ga.lbs.se>
2018-09-06 11:24:32 +0200

    FredriksStuff
    Shooting är gjort med ett target för att sikta

---
+++ Game 1.0/Assets/Scripts/Aimtarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aimtarget : MonoBehaviour
+{
+    public Vector3 TargetPos;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TargetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        TargetPos.z = 0;
+        GetComponent<Transform>().position = TargetPos;
+    }
+}
---
+++ Game 1.0/Assets/Scripts/Shoot.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Vector2 mousePos;
+    public GameObject Projectile;
+    public float resetCooldown = 1f;
+    private float cooldown;
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        cooldown -= Time.deltaTime;
+
+        // The projektile for the ranged attacks
+        GameObject newProjectile;
+
+
+        mousePos = Input.mousePosition;
+        Vector3 MouseWorldPos;
+
+        // finds the mouse location inside the camera
+        MouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 differens;
+        differens = MouseWorldPos - transform.position;
+        differens.Normalize();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (cooldown <= 0)
+            {
+                newProjectile = Instantiate(Projectile, transform.position, Quaternion.identity);
+
+                newProjectile.GetComponent<Rigidbody2D>().AddForce(differens * 20, ForceMode2D.Impulse);
+                cooldown = resetCooldown;
+                Physics2D.IgnoreCollision(newProjectile.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            }
+        }
+    }
+}
